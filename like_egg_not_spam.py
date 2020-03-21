5bd12442c6266ccafeb4d1d347c368d3cec70335
Daky <mail@sebfrodin.se>
2020-03-21 23:16:33 +0100

    Create like_egg_not_spam.py

---
+++ like_egg_not_spam.py
@@ -0,0 +1,31 @@
+
+
+
+
+test = first_string, second_string, third_string = "Jag tYcker om äGg", "inte", "SPAM"
+
+print(str(test).split())
+
+### V1
+split_first_string = first_string.title().swapcase().rsplit(" ", 2)
+compiled_string = (f"{split_first_string[0]} {second_string.title().swapcase()} {split_first_string[1]} {third_string}")
+print(compiled_string)
+
+### V2
+list_index = [0, 1, 4 , 2, 5]
+
+convert_to_list = (
+    f"""
+    {first_string}
+    {second_string}
+    """.title().swapcase() 
+    + third_string
+).split()
+
+list_wanted_items = [convert_to_list[i] for i in list_index]
+convert_to_string = (' '.join(list_wanted_items))
+print(convert_to_string)
+
+### V3
+initial_list = ((f"{first_string} {second_string} ").title().swapcase() + third_string).split()
+print(' '.join([initial_list[i] for i in [0,1,4,2,5]]))
